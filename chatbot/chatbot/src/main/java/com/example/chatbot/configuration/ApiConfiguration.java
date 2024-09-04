fcb9ff388194b6c510b7d671755ceb73cf7742db
Francisco Fernandez Ruiz <francisco.fernandez.r@altia.es>
2024-09-04 13:26:59 +0200

    añadida clase configuracion

---
+++ chatbot/chatbot/src/main/java/com/example/chatbot/configuration/ApiConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.chatbot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:pass.properties")
+public class ApiConfiguration {
+
+}
